4d62fc3795ead152da5a70873979296e08248123
Felipe <felipevoidela@gmail.com>
2023-04-04 22:04:29 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 //exercicio1
-let num = undefined
+let num 
 document.write(num)
 document.write("<br><br>")
-
-//exercicio2
-let num1 = 10
-let num2 = 5
-document.write(num1 % num2)
-
-//exercicio 3
-let peso =prompt("Digite seu peso")
-let altura =prompt("Digite sua altura")
-let imc =(peso / altura) ;
-if (imc <= 18.5) {
-    document.write("abaixo do peso") 
-}else if (imc >= 18.5 || imc <= 24.9) {
-    document.write("peso ideal") 
-}else if(imc >= 24.9) {
-    document.write("acima do peso") 
-}
 
