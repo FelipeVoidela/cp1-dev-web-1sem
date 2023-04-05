313c073216982bc254445e6368d08e564d254dfb
Felipe <felipevoidela@gmail.com>
2023-04-04 22:39:08 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ document.write("<br><br>")
 //exercicio 3
 let peso =prompt("Digite seu peso")
 let altura =prompt("Digite sua altura")
-let imc =(peso / altura) ;
+let imc =(peso / (altura * altura)) ;
 if (imc <= 18.5) {
     document.write("abaixo do peso") 
     document.write("<br><br>")
-}else if (imc >= 18.5 || imc <= 24.9) {
+}else if (imc >= 18.5  && imc <= 24.9) {
     document.write("peso ideal") 
     document.write("<br><br>")
 }else if(imc >= 24.9) {
@@ -31,13 +31,30 @@ let idade =prompt("Digite sua idade")
 if(idade <=12){
     document.write("é uma criança")
     document.write("<br><br>")
-}else if (idade >= 13 || idade <= 18){
+}else if (idade >= 13 && idade <= 18){
     document.write("é um adolescente")
     document.write("<br><br>")
-}else if(idade >= 19 || idade <= 60){
+}else if(idade >= 19 && idade <= 60){
     document.write("é uma adulto")
     document.write("<br><br>")
 }else if (idade >= 60) {
     document.write("é um idoso")
     document.write("<br><br>")
 }
+
+//exercicio 5
+
+let usuário
+let senha = 1234
+let senha2 =prompt("Digite  seu senha")
+if(senha2 == senha){
+    document.write("login foi realizado com sucesso")
+    document.write("<br><br>")
+}else if(senha2 != senha){
+    document.write("houve falha de autenticação")
+    document.write("<br><br>")
+}
+
+//exercicio 6
+
+
